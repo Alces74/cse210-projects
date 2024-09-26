f2527c872b08a32d1d64d616bd252464913dde2c
gskoy <grantskoy@gmail.com>
2024-09-26 11:04:25 -0600

    Activity 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Teacher";
+        job1._company = "Shasta High School";
+        job1._startYear = 2015;
+        job1._endYear = 2017;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Netflix";
+        job2._startYear = 2017;
+        job2._endYear = 2024;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Grant Skoy";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
     }
 }
